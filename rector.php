ff11f7a81f6e470097eba31a2532fbedb72e7b6a
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-11-17 01:36:25 +0300

    Installed Rector

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->parallel();
+    $rectorConfig->cacheDirectory(__DIR__ . '/var/rector');
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_82,
+        // PHPUnitSetList::PHPUNIT_100, https://github.com/sebastianbergmann/phpunit/issues/5513
+    ]);
+};
