bc64cea85cc470c65210c26e408e6890ee96ee2b
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-11-16 21:35:29 +0300

    Added SymfonyVarDumperServiceDumperTest

--- src/ServiceDumper/SymfonyVarDumperServiceDumper.php
+++ src/ServiceDumper/SymfonyVarDumperServiceDumper.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 namespace PHPyh\ServiceDumperBundle\ServiceDumper;
 
 use PHPyh\ServiceDumperBundle\ServiceDumper;
-use Symfony\Component\VarDumper\VarDumper;
+use Symfony\Component\VarDumper\Cloner\ClonerInterface;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Dumper\DataDumperInterface;
 
 /**
  * @api
- * @codeCoverageIgnore
  */
 final readonly class SymfonyVarDumperServiceDumper implements ServiceDumper
 {
+    public function __construct(
+        private DataDumperInterface $dumper = new CliDumper(),
+        private ClonerInterface $cloner = new VarCloner(),
+    ) {}
+
     public function dump(array $servicesById): void
     {
         foreach ($servicesById as $id => $service) {
-            /** @psalm-suppress TooManyArguments */
-            VarDumper::dump($service, $id);
+            $this->dumper->dump($this->cloner->cloneVar($service)->withContext(['label' => $id]));
         }
     }
 }
---
+++ tests/ServiceDumper/SymfonyVarDumperServiceDumperTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPyh\ServiceDumperBundle\ServiceDumper;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+
+#[CoversClass(SymfonyVarDumperServiceDumper::class)]
+final class SymfonyVarDumperServiceDumperTest extends TestCase
+{
+    public function testItDumpsIdsAndObjects(): void
+    {
+        $dumper = new SymfonyVarDumperServiceDumper(dumper: new CliDumper('php://output'));
+        $servicesById = [
+            'service_a_id' => new \ArrayObject(['Something inside service a.']),
+            'service_b_id' => new \ArrayObject(['Something inside service b.']),
+        ];
+
+        ob_start();
+        $dumper->dump($servicesById);
+        $output = ob_get_clean();
+
+        self::assertStringContainsString('service_a_id', $output);
+        self::assertStringContainsString('Something inside service a.', $output);
+        self::assertStringContainsString('service_b_id', $output);
+        self::assertStringContainsString('Something inside service b.', $output);
+    }
+}
