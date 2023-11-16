a27f300b029ac5676e4fc8984a3bf2537c3581f9
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-11-17 01:36:10 +0300

    Improved .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,13 +5,9 @@ declare(strict_types=1);
 use PHPyh\CodingStandard\PhpCsFixerCodingStandard;
 
 $finder = PhpCsFixer\Finder::create()
-    ->in([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ])
-    ->append([
-        __FILE__,
-    ]);
+    ->in(__DIR__)
+    ->exclude('var')
+    ->append([__FILE__]);
 
 $config = (new PhpCsFixer\Config())
     ->setFinder($finder)
