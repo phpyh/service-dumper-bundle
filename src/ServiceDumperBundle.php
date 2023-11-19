163bf8ab646e5813171f0cb4051223a6c41f1065
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-11-20 00:45:53 +0300

    Refactored ServiceDumperBundle to extend Bundle to support Symfony 5

--- src/ServiceDumperBundle.php
+++ src/ServiceDumperBundle.php
@@ -11,23 +11,25 @@ use PHPyh\ServiceDumperBundle\ServiceDumper\SymfonyVarDumperServiceDumper;
 use PHPyh\ServiceDumperBundle\ServiceDumper\VarDumpServiceDumper;
 use PHPyh\ServiceDumperBundle\ServiceDumper\XdebugServiceDumper;
 use PHPyh\ServiceDumperBundle\ServiceFinder\BasicServiceFinder;
-use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\ServiceLocator;
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\VarDumper\VarDumper;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\inline_service;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 /**
+ * AbstractBundle is not used to keep compatibility with Symfony 5.
+ *
  * @api
  */
-final class ServiceDumperBundle extends AbstractBundle
+final class ServiceDumperBundle extends Bundle implements ConfigurationInterface
 {
-    protected string $extensionAlias = 'phpyh_service_dumper';
-
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new CollectPrivateServicesPass(), PassConfig::TYPE_BEFORE_REMOVING, -32);
@@ -35,11 +37,13 @@ final class ServiceDumperBundle extends AbstractBundle
     }
 
     /**
-     * @psalm-suppress UndefinedMethod, MixedMethodCall
+     * @psalm-suppress UndefinedMethod, MixedMethodCall, PossiblyNullReference
      */
-    public function configure(DefinitionConfigurator $definition): void
+    public function getConfigTreeBuilder(): TreeBuilder
     {
-        $definition->rootNode()
+        $treeBuilder = new TreeBuilder('phpyh_service_dumper');
+
+        $treeBuilder->getRootNode()
             ->children()
                 ->scalarNode('service_dumper')
                     ->info(sprintf(
@@ -56,31 +60,51 @@ final class ServiceDumperBundle extends AbstractBundle
                     ->defaultValue('basic')
                 ->end()
             ->end();
+
+        return $treeBuilder;
     }
 
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    public function getContainerExtension(): ?ExtensionInterface
     {
-        /** @var array{service_dumper: string, service_finder: string} $config */
-        $container->services()
-            ->set('phpyh.service_dumper.private_services', ServiceLocator::class)
-                ->args([[]])
-            ->set(DebugDumpServiceCommand::class)
-                ->args([
-                    inline_service(AllServicesContainer::class)->args([
-                        service('service_container'),
-                        service('phpyh.service_dumper.private_services'),
-                    ]),
-                    match ($config['service_dumper']) {
-                        'var_dump' => inline_service(VarDumpServiceDumper::class),
-                        'symfony_var_dumper' => inline_service(SymfonyVarDumperServiceDumper::class),
-                        'xdebug' => inline_service(XdebugServiceDumper::class),
-                        default => service($config['service_dumper']),
-                    },
-                    match ($config['service_finder']) {
-                        'basic' => inline_service(BasicServiceFinder::class),
-                        default => service($config['service_finder']),
-                    },
-                ])
-                ->tag('console.command');
+        return new class ($this) extends Extension {
+            public function __construct(
+                private readonly ServiceDumperBundle $bundle,
+            ) {}
+
+            public function getAlias(): string
+            {
+                return 'phpyh_service_dumper';
+            }
+
+            public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
+            {
+                return $this->bundle;
+            }
+
+            public function load(array $configs, ContainerBuilder $container): void
+            {
+                /** @var array{service_dumper: string, service_finder: string} $config */
+                $config = $this->processConfiguration($this->bundle, $configs);
+                $container->register('phpyh.service_dumper.private_services', ServiceLocator::class)->setArguments([[]]);
+                $container->register(DebugDumpServiceCommand::class)
+                    ->setArguments([
+                        (new Definition(AllServicesContainer::class))->setArguments([
+                            new Reference('service_container'),
+                            new Reference('phpyh.service_dumper.private_services'),
+                        ]),
+                        match ($config['service_dumper']) {
+                            'var_dump' => new Definition(VarDumpServiceDumper::class),
+                            'symfony_var_dumper' => new Definition(SymfonyVarDumperServiceDumper::class),
+                            'xdebug' => new Definition(XdebugServiceDumper::class),
+                            default => new Reference($config['service_dumper']),
+                        },
+                        match ($config['service_finder']) {
+                            'basic' => new Definition(BasicServiceFinder::class),
+                            default => new Reference($config['service_finder']),
+                        },
+                    ])
+                    ->addTag('console.command');
+            }
+        };
     }
 }
