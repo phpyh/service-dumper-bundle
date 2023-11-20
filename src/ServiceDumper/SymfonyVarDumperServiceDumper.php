93a6b2dd6fb794e8d1b62a39c28d339a4a243054
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-11-20 03:35:11 +0300

    Use ScalarStub in SymfonyVarDumperServiceDumper to check for label support

--- src/ServiceDumper/SymfonyVarDumperServiceDumper.php
+++ src/ServiceDumper/SymfonyVarDumperServiceDumper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PHPyh\ServiceDumperBundle\ServiceDumper;
 
 use PHPyh\ServiceDumperBundle\ServiceDumper;
+use Symfony\Component\VarDumper\Caster\ScalarStub;
 use Symfony\Component\VarDumper\Cloner\ClonerInterface;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\CliDumper;
@@ -20,10 +21,22 @@ final class SymfonyVarDumperServiceDumper implements ServiceDumper
         private readonly ClonerInterface $cloner = new VarCloner(),
     ) {}
 
+    private static function supportsLabels(): bool
+    {
+        // Both labels and ScalarStub appeared in 6.3.
+        return class_exists(ScalarStub::class);
+    }
+
     public function dump(array $servicesById): void
     {
-        foreach ($servicesById as $id => $service) {
-            $this->dumper->dump($this->cloner->cloneVar($service)->withContext(['label' => $id]));
+        if (self::supportsLabels()) {
+            foreach ($servicesById as $id => $service) {
+                $this->dumper->dump($this->cloner->cloneVar($service)->withContext(['label' => $id]));
+            }
+
+            return;
         }
+
+        $this->dumper->dump($this->cloner->cloneVar($servicesById));
     }
 }
